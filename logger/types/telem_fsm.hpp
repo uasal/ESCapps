8108ab225f539e57289574b93a3ff9de9d053263
Irina Stefan <istefan@arizona.edu>
2024-09-25 16:19:01 -0700

    Matching updates in fsmCtrl app

--- logger/types/telem_fsm.hpp
+++ logger/types/telem_fsm.hpp
@@ -10,8 +10,8 @@
 #ifndef logger_types_telem_fsm_hpp
 #define logger_types_telem_fsm_hpp
 
-#include "flatbuffer_log.hpp" // Has to be this path. It references the folder where the XWC build will install it.
 #include "generated/telem_fsm_generated.h"
+#include "flatbuffer_log.hpp" // Has to be this path. It references the folder where the XWC build will install it.
 
 namespace MagAOX
 {
